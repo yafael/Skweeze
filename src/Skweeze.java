75b687a18ffa994aae7bedd0e938bc59a221c96f
int1 <int1@hp>
2017-02-11 14:17:42 -0500

    edited nlc test; added alchemy test

--- src/Skweeze.java
+++ src/Skweeze.java
@@ -1,6 +1,7 @@
 import watson_services.*;
 
 public class Skweeze {
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		DocConvert dc = new DocConvert();
 		dc.convert("data/CV.pdf");
--- test/lib_test/NLC_Test.java
+++ test/lib_test/NLC_Test.java
@@ -5,17 +5,19 @@ import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLang
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.*;
 import watson_services.Credentials;
 
+@SuppressWarnings("unused")
 public class NLC_Test {
 	public static void main(String[] args) throws IOException {
 		Credentials creds = Credentials.loadCreds("credentials/nlc_cred");
 		NaturalLanguageClassifier service = new NaturalLanguageClassifier();
 		service.setUsernameAndPassword(creds.getUsername(), creds.getPassword());
 		//Create classifier
-		Classifier classifier = service.createClassifier("test","en",
-				new File("data/input_data.csv")).execute();
-		System.out.println(classifier);
+//		Classifier classifier = service.createClassifier("test","en",
+//				new File("data/new_data.csv")).execute();
+//		System.out.println(classifier);
 		//Query status (training, available, etc) TODO decide how to get classifier ID
-//		Classifier classifier = service.getClassifier("f5b432x172-nlc-2347").execute();
+		Classifier classifier = service.getClassifiers().execute().getClassifiers().get(0);
+		System.out.println(classifier);
 		//Classify sentence
 //		Classification classification = service.classify(
 //				classifier.getId(),"Will it rain Saturday?").execute();
