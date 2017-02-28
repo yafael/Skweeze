a19eb00fa7d1a023838e7998aaca2354bc94c8c1
NicholasChehade <chehade.2@osu.edu>
2017-02-28 18:54:45 -0500

    R and R with SolrQuery (may still have issues)

--- src/watson_services/RandR.java
+++ src/watson_services/RandR.java
@@ -1,20 +1,60 @@
 package watson_services;
 import java.io.IOException;
+import java.net.URI;
 
 import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.*;
 import com.ibm.watson.developer_cloud.retrieve_and_rank.v1.model.SolrCluster;
 
+import okhttp3.Request;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+
+
 public class RandR {
-	
-	private RetrieveAndRank service;
-	private String clusterID = "sccf7db882_4e76_49c0_bb10_0d484b286016";
-	
+
+	private static RetrieveAndRank service;
+	private static String clusterID = "sccf7db882_4e76_49c0_bb10_0d484b286016";
+	private static HttpSolrClient solrClient;
+
+	@SuppressWarnings("deprecation")
+	private static HttpSolrClient getSolrClient(String uri, String username, String password) {
+	    return new HttpSolrClient(service.getSolrUrl(clusterID), createHttpClient(uri, username, password));
+	}
+	private static HttpClient createHttpClient(String uri, String username, String password) {
+	    final URI scopeUri = URI.create(uri);
+
+	    final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+	    credentialsProvider.setCredentials(new AuthScope(scopeUri.getHost(), scopeUri.getPort()),
+	        new UsernamePasswordCredentials("{username}", "{password}"));
+
+	    final HttpClientBuilder builder = HttpClientBuilder.create()
+	        .setMaxConnTotal(128)
+	        .setMaxConnPerRoute(32)
+	        .setDefaultRequestConfig(RequestConfig.copy(RequestConfig.DEFAULT).setRedirectsEnabled(true).build())
+	        .setDefaultCredentialsProvider(credentialsProvider);
+	    return builder.build();
+	}
 	public RandR() throws IOException{
 		Credentials creds = Credentials.loadCreds("credentials/randr_cred");
 		service = new RetrieveAndRank();
 		service.setUsernameAndPassword(creds.getUsername(), creds.getPassword());
 	}
-	
-	public void rank(){
+
+	public void rank() throws IOException, SolrServerException{
+		Credentials creds = Credentials.loadCreds("credentials/randr_cred");
+		solrClient = getSolrClient(service.getSolrUrl(clusterID),creds.getUsername(),creds.getPassword());
+		SolrQuery query = new SolrQuery("computer");
+		QueryResponse response = solrClient.query("Job-Postings", query);
+		System.out.println(response);
+		System.out.println(service.getRankers().execute());
 	}
 }
